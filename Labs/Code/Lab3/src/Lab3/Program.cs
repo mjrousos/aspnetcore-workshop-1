6b9b2ab8d0a0eb94a760f7cfc0f2e0d1992a36f3
Mike Rousos <Mike@MJRousos.com>
2017-10-12 15:22:38 -0400

    Update Lab3 for ASP.NET Core 2.0

--- Labs/Code/Lab3/src/Lab3/Program.cs
+++ Labs/Code/Lab3/src/Lab3/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Lab3
@@ -11,15 +7,13 @@ namespace Lab3
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls("http://localhost:8081")
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:8081")
+                .Build();
     }
 }
